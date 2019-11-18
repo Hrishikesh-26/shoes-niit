77ff6e626f88a98f19b7ee33aa0df1e222414958
Mandar <Mandar@MandarLaptop>
2019-11-18 18:12:41 +0530

    Check database password before executing
    Deployed and tested.

--- src/main/java/controller/register.java
+++ src/main/java/controller/register.java
@@ -27,7 +27,7 @@ public class register extends HttpServlet
 		try 
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/shoes", "root", "hrishi");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/shoes", "root", "root");
 	        PreparedStatement ps=con.prepareStatement("insert into user (username,email,password) values (?,?,?);");
 	        ps.setString(1, user.getUsername());
 	        ps.setString(2, user.getEmail());
